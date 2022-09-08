776e8cbee44049dfdb9d904b4f58e2c06ab6c1ef
seppaemi <emilia.seppala@helsinki.fi>
2022-09-08 22:09:12 +0300

    commit first repos

---
+++ src/UI/game_UI.py
@@ -0,0 +1,68 @@
+import pygame
+from load_image import load_image
+class Game_UI:
+    def __init__(self):
+        self.place_size = 100
+        self.screen_height = self.place_size * 7
+        self.screen_width = self.place_size * 7
+        self.screen = None
+        self.red_piece = load_image("red.png")
+        self.green_piece = load_image("green.png")
+        self.green_played = load_image("green_played.png")
+        self.red_played = load_image("red_played.png")
+        self.empty = load_image("empty.png")
+
+    def game_UI_setup(self, title):
+        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+        pygame.display.set_caption(f"CONNECT FOUR - {title}")
+        self.font = pygame.font.SysFont("Helvetica", 45, 1)
+
+    def draw_victory(self, green_won, red_won):
+        if green_won:
+            green_text = self.font.render("VIHREÄ VOITTI!", True, (255, 255, 0))
+            self.screen.blit(green_text, (150,25))
+        if red_won:
+            red_text = self.font.render("PUNAINEN VOITTI!", True, (255, 0, 0))
+            self.screen.blit(red_text, (150,25))
+
+    def draw_tie(self):
+        tie_text = self.font.render("TASAPELI", True, (255,255,255))
+        self.screen.blit(tie_text, (250,25))
+
+    def draw_piece(self, green_turn, mouse_pos):
+        x = mouse_pos[0]
+        x-=50
+        if x < 0:
+            x = 0
+        if x >= 600:
+            x = 600
+        if green_turn:
+            self.screen.blit(self.green_piece, (x,0))
+        else:
+            self.screen.blit(self.red_piece, (x,0))
+
+    def correct_image(self, place):
+        if place == 0:
+            return self.empty
+        if place == 1:
+            return self.green_played
+        if place == 2:
+            return self.red_played
+    
+    def draw_screen(self, board, game_over, tie_game, green_won,
+                    red_won, green_turn, mouse_pos):
+        self.screen.fill((22,8,91))
+        for y_value in range(6):
+            for x_value in range(7):
+                image = self.correct_image(board[y_value][x_value])
+                self.screen.blit(image, (x_value*self.place_size, y_value*self.place_size+100))
+        if game_over:
+            if not tie_game:
+                self.draw_victory(green_won, red_won)
+            else:
+                self.draw_tie()
+        if not game_over:
+            self.draw_piece(green_turn, mouse_pos)
+            pygame.display.update()
+        pygame.display.flip()
+    
---
+++ src/modules/gameplay.py
@@ -0,0 +1,78 @@
+import numpy
+
+ORDER = [3,2,4,1,5,0,6]
+
+def check_tie(board):
+    full = True
+    for x in range(0,7):
+            if not check_column_full(board, x):
+                full = False
+    return full
+
+def check_if_four(line):
+    if line.count(1) == 4:
+        return 1
+    if line.count(2) == 4:
+        return 2
+    return -1
+
+def check_win(board):
+    hor = check_horizontal(board)
+    vert = check_vertical(board)
+    up_dia = check_up_diagonal(board)
+    do_dia = check_down_diagonal(board)
+    if hor == 1 or vert == 1 or up_dia == 1 or do_dia == 1:
+        return 1
+    if hor == 2 or vert == 2 or up_dia == 2 or do_dia == 2:
+        return 2
+    return 0
+
+def check_horizontal(board):
+    for y in range(5,-1,-1):
+        row = board[y][:]
+        for x in range(6,2,-1):
+            hor = [row[x-3],row[x-2],row[x-1],row[x]]
+            if check_if_four(hor) == 2:
+                return 2
+            if check_if_four(hor) == 1:
+                return 1
+    return 0
+
+def check_vertical(board):
+    n_board = numpy.array(board)
+    for x in ORDER:
+        col = n_board[:,x]
+        for y in range(5,2,-1):
+            vert=[col[y-3],col[y-2],col[y-1],col[y]]
+            if check_if_four(vert) == 2:
+                return 2
+            if check_if_four(vert) == 1:
+                return 1
+    return 0
+
+def check_up_diagonal(board):
+    for y in range(5,2,-1):
+        for x in range(3,-1,-1):
+            up_dia = [board[y][x],board[y-1][x+1],
+                        board[y-2][x+2],board[y-3][x+3]]
+            if check_if_four(up_dia) == 2:
+                return 2
+            if check_if_four(up_dia) == 1:
+                return 1
+    return 0
+
+def check_down_diagonal(board):
+    for y in range(5,2,-1):
+        for x in range(3,7):
+            do_dia = [board[y-3][x-3], board[y-2][x-2],
+                        board[y-1][x-1], board[y][x]]
+            if check_if_four(do_dia) == 2:
+                return 2
+            if check_if_four(do_dia) == 1:
+                return 1
+    return 0
+
+def check_column_full(board, x):
+    if board[0][x] != 0:
+        return True
+    return False
